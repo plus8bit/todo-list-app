f18c3396d26e4de0697abe479a415a1250a73a28
Nick <plus0bit@gmail.com>
2019-03-31 13:58:03 +0300

    Initial xcode project

---
+++ Todo List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todo List
+//
+//  Created by Nick on 31/03/2019.
+//  Copyright © 2019 Nick Danilov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
